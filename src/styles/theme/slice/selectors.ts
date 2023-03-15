57b9d4e730988f6a8c14ed02d4f4c9539d13a8dd
Mike Pechousek <mike.pechousek@gmail.com>
2023-03-14 23:16:45 -0400

    remove import

--- src/styles/theme/slice/selectors.ts
+++ src/styles/theme/slice/selectors.ts
@@ -3,7 +3,6 @@ import { createSelector } from '@reduxjs/toolkit';
 import { RootState } from 'types';
 import { initialState } from '.';
 import { themes } from '../themes';
-import { isSystemDark } from '../utils';
 
 export const selectTheme = createSelector(
   [(state: RootState) => state.theme || initialState],
